82654c01018d8140c80f44f2bd35834de98e251a
linzhouzhi <490664646@qq.com>
2017-03-23 00:01:09 +0800

    fda

--- public/js/footer.js
+++ public/js/footer.js
@@ -2,11 +2,12 @@
  * Created by lzz on 17/3/15.
  */
 $(document).ready(function () {
-    // 加载效果
+    /* 加载效果
     $(".iframe-load:visible").fakeLoader({
         timeToHide:800,
         bgColor:"#ecf0f5",
         spinner:"spinner1"
     });
+    */
 
 });
